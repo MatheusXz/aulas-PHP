f30d16ffc222b4e76f69a51f71f55ee7ddba819d
MatheusXz <matheus000000018@gmail.com>
2023-05-31 21:36:36 -0300

    up

---
+++ library/pages/conn/index.php
@@ -0,0 +1,50 @@
+<?php
+
+$config = array(
+    'host' => '10.67.22.216',
+    'dbname' => 's222_matheus35',
+    'user' => 's222_bda',
+    'pass' => 's22022',
+);
+
+// $config = array(
+//     'host' => '10.0.0.101',
+//     'dbname' => 'loja_de_carros',
+//     'user' => 'root',
+//     'pass' => 'some_pass',
+// );
+
+function exitSession($loc)
+{
+    session_start();
+    session_destroy();
+    header($loc);
+}
+
+function HeaderLocal($loc)
+{
+    header($loc);
+}
+function conectar($config)
+{
+    try {
+        $dsn = 'mysql:host=' . $config['host'] . ';dbname=' . $config['dbname'] . ';charset=utf8';
+        $pdo = new PDO($dsn, $config['user'], $config['pass']);
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        return $pdo;
+    } catch (PDOException $e) {
+        die('Erro ao conectar ao banco de dados: ' . $e->getMessage());
+    }
+}
+function getCompradorSaldo($connect, $idCompradorLogado)
+{
+    $querySaldoComprador = 'SELECT user_saldo FROM usuarios_car WHERE id = :idCompradorLogado';
+    $stmthSaldoComprador = $connect->prepare($querySaldoComprador);
+    $stmthSaldoComprador->bindValue(":idCompradorLogado", $idCompradorLogado);
+    $stmthSaldoComprador->execute();
+    $resultSaldoComprador = $stmthSaldoComprador->fetch(PDO::FETCH_ASSOC);
+    $saldoComprador = $resultSaldoComprador['user_saldo'];
+    return $saldoComprador;
+}
+
+$connect = conectar($config);
--- loja-de-veiculos/php/login.php
+++ loja-de-veiculos/php/login.php
@@ -87,12 +87,12 @@ if (isset($_POST['cadastrar'])) {
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
   <link href='https://unpkg.com/boxicons@2.1.2/css/boxicons.min.css' rel='stylesheet'>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-  <link href="../css/main.css" rel="stylesheet" >
+  <link href="../css/main.css" rel="stylesheet">
 
 </head>
 
 <body>
-    <?php echo $div_message;?>
+  <?php echo $div_message; ?>
   <div class="forms row container_login">
     <div class="form sign-up">
       <form action="" method="post">
