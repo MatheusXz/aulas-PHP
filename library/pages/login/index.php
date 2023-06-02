65405b9c80fe033eeeccdf8a9520f2775d640346
Matheus Aureliano <matheus000000018@gmail.com>
2023-06-02 01:44:50 -0300

    up

--- library/pages/login/index.php
+++ library/pages/login/index.php
@@ -88,53 +88,84 @@ if (isset($_POST['cadastrar'])) {
     <link href='https://unpkg.com/boxicons@2.1.2/css/boxicons.min.css' rel='stylesheet'>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link href="../../css/main.css" rel="stylesheet">
-
+    <style>
+        body {
+            /* height: 100vh; */
+            background-image: url('https://cdn.wallpapersafari.com/91/90/Bmhy8U.jpg');
+            /* background-size: cover; */
+            background-size: cover;
+            background-position: center;
+            background-repeat: no-repeat;
+            background-attachment: fixed;
+            -webkit-background-size: auto;
+            -moz-background-size: auto;
+            -o-background-size: auto;
+            /* Centraliza verticalmente */
+        }
+    </style>
 </head>
 
-<body style="background-color: black;">
+<body>
     <?php echo $div_message; ?>
-    <div class="forms row container_login">
-        <div class="form sign-up">
-            <form action="" method="post">
-                <h2 class="text-center">Cadastro</h2>
-                <div class="form-group">
-                    <!-- NOME -->
-                    <label for="nome">Nome:</label>
-                    <input type="text" class="form-control" id="nome" name="nome" placeholder="Ex: Carlos" required pattern=".{3,}">
-                    <!-- SOBRENOME -->
-                    <label for="sobrenome">Sobrenome:</label>
-                    <input type="text" class="form-control" id="sobrenome" name="sobrenome" placeholder="Ex: Silva Soares" required pattern=".{3,}">
-                    <!-- EMAIL -->
-                    <label for="email">E-mail:</label>
-                    <input type="email" class="form-control" id="email" name="email" placeholder="name@exemplo.com" required pattern=".{7,}">
-                    <!-- SENHA -->
-                    <label for="SENHA">Senha:</label>
-                    <input type="password" class="form-control" id="senha" name="senha" placeholder="*********" required pattern=".{8,}">
-                    <div class="my-5 d-flex justify-content-between">
-                        <button class="btn text-muted btn-outline-light link ">Já tenho uma conta</button>
-                        <button class="btn btn-primary criar_user" name="cadastrar">Cadastrar</button>
-                    </div>
+
+
+    <div class="container">
+        <!-- Title -->
+
+        <!-- insertion section -->
+        <div class="">
+            <div class="forms row d-flex justify-content-center">
+                <div class=" form sign-up " style="background: rgba(0, 0, 0, 0.75);">
+                    <form action="" method="post">
+                        <div class="row">
+                            <h1 class="text-primary text-center">Library System</h1>
+                        </div>
+                        <h5 class="text-center mt-5">Cadastro</h5>
+                        <div class="form-group">
+                            <!-- NOME -->
+                            <label for="nome">Nome:</label>
+                            <input type="text" class="form-control" id="nome" name="nome" placeholder="Ex: Carlos" required pattern=".{3,}">
+                            <!-- SOBRENOME -->
+                            <label for="sobrenome">Sobrenome:</label>
+                            <input type="text" class="form-control" id="sobrenome" name="sobrenome" placeholder="Ex: Silva Soares" required pattern=".{3,}">
+                            <!-- EMAIL -->
+                            <label for="email">E-mail:</label>
+                            <input type="email" class="form-control" id="email" name="email" placeholder="name@exemplo.com" required pattern=".{7,}">
+                            <!-- SENHA -->
+                            <label for="SENHA">Senha:</label>
+                            <input type="password" class="form-control" id="senha" name="senha" placeholder="*********" required pattern=".{8,}">
+                            <div class="my-5 d-flex justify-content-between">
+                                <button class="btn text-muted btn-outline-light link ">Já tenho uma conta</button>
+                                <button class="btn btn-primary criar_user" name="cadastrar">Cadastrar</button>
+                            </div>
+                        </div>
+                    </form>
                 </div>
-            </form>
-        </div>
-        <div class="form sign-in">
-            <form action="" method="post">
-                <h2 class="text-center">Login</h2>
-                <div class="form-group">
-                    <!-- EMAIL -->
-                    <label for="email">E-mail:</label>
-                    <input type="email" class="form-control" id="myTextField" name="email" placeholder="name@exemplo.com" required pattern=".{7,}">
-                    <!-- SENHA -->
-                    <label for="SENHA">Senha:</label>
-                    <input type="password" class="form-control" id="senha" name="senha" placeholder="*********" required pattern=".{8,}">
-                    <div class="my-5 d-flex justify-content-between">
-                        <button class="btn text-muted btn-outline-light link">Criar uma nova conta</button>
-                        <button class="btn btn-primary login_user" name="login_user">Entrar</button>
-                    </div>
+                <div class="form sign-in">
+                    <form action="" method="post">
+                        <div class="row">
+                            <h1 class="text-center text-primary">Library System</h1>
+                        </div>
+                        <h5 class="text-center  mt-5">Login</h5>
+                        <div class="form-group">
+                            <!-- EMAIL -->
+                            <label for="email">E-mail:</label>
+                            <input type="email" class="form-control" id="myTextField" name="email" placeholder="name@exemplo.com" required pattern=".{7,}">
+                            <!-- SENHA -->
+                            <label for="SENHA">Senha:</label>
+                            <input type="password" class="form-control" id="senha" name="senha" placeholder="*********" required pattern=".{8,}">
+                            <div class="my-5 d-flex justify-content-between">
+                                <button class="btn text-muted btn-outline-light link">Criar uma nova conta</button>
+                                <button class="btn btn-primary login_user" name="login_user">Entrar</button>
+                            </div>
+                        </div>
+                    </form>
                 </div>
-            </form>
+            </div>
         </div>
     </div>
+
+
 
     <script>
         const forms = document.querySelector(".forms"),
@@ -191,9 +222,12 @@ if (isset($_POST['cadastrar'])) {
 
 
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous">
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous">
+    </script>
 </body>
 
 </html>
